436e1e4cca4227789be9694dbe0bfa3d82a76cbb
Akropot <133800117+Toporka@users.noreply.github.com>
2025-05-15 12:30:22 +0300

    Add files via upload

---
+++ ProjectStructureDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("Logs/MyLog.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+
+UserService service = new UserService();
+
+User anton = new User(1212, "Антон");
+User alex = new User(3243, "Алекс");
+
+service.AddUser(anton);
+service.AddUser(alex);
+
+User user = service.GetUser(1212);
+Log.Information($"\nID: {user.Id}\nName: {user.Name}");
---
+++ ProjectStructureDemo/Services/UserService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class UserService
+{
+    // Список пользователей
+    List<User> users;
+
+    // Конструктор класса с выделением памяти для списка
+    public UserService() => users = new List<User>();
+
+    // Метод добавления нового объекта
+    public void AddUser(User user) => users.Add(user);
+
+    // Метод нахождения объекта по свойству Id
+    public User GetUser(int Id)
+    {
+        foreach (var user in users)
+            if (user.Id == Id) 
+                return user;
+
+        return null;
+    }
+}
